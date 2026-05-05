1a82de8242ba2c7c01ad9b027e8bd54e284cb181
con6cao <caio_vinicius2246@hotmail.com>
2026-05-04 22:25:42 -0300

    configuracao de deploy 13

--- src/main/java/com/univesp/supremeribs/repository/CardapioRepository.java
+++ src/main/java/com/univesp/supremeribs/repository/CardapioRepository.java
@@ -1,6 +1,5 @@
 package com.univesp.supremeribs.repository;
 
-import com.univesp.supremeribs.model.BaseFile;
 import com.univesp.supremeribs.model.Cardapio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
